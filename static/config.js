abbcb5e2d6e4976497c2d406311335e053106a2e
nLininger <njl43@pitt.edu>
2025-03-12 15:37:40 -0400

    Completed js client-side frontend

--- static/config.js
+++ static/config.js
@@ -14,6 +14,6 @@
  * @type {{apiKey: string, authDomain: string}}
  */
 const config = {
-  apiKey: 'AIzaSyDhwsk8AklD8Ipj26fVzYTMgrWvE8x1rgc',
-  authDomain: 'cc-spring2025.firebaseapp.com',
+  apiKey: 'AIzaSyDboeb0oC-30asZaTck001DXNqNu2ikmfA',
+  authDomain: 'silent-emissary-451416-u5.firebaseapp.com',
 };
